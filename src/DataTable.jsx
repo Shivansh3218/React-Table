4085b01b0321d8d0c2f61b25b2be37327240bcd6
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-12-04 17:19:32 +0530

    second commit

--- src/DataTable.jsx
+++ src/DataTable.jsx
@@ -10,9 +10,8 @@ import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import Button from "@mui/material/Button";
-import ImportExportIcon from '@mui/icons-material/ImportExport';
 
-import './index.css'
+import "./index.css";
 
 const style = {
   position: "absolute",
@@ -28,11 +27,16 @@ const style = {
   pb: 3,
 };
 
-
 export default function DataTable() {
   const [open, setOpen] = useState(false);
   let [columnName, setColumnName] = useState("");
-  const [columns, setColumns] = useState(["ID", "Course", "Price", "Rating","Action"]);
+  const [columns, setColumns] = useState([
+    "ID",
+    "Course",
+    "Price",
+    "Rating",
+    "Action",
+  ]);
   let [rows, setRows] = useState([
     { id: 1, course: "Java", price: 20, rating: 5 },
     { id: 2, course: "C++", price: 20, rating: 2 },
@@ -46,69 +50,88 @@ export default function DataTable() {
     { id: 10, course: "CSS", price: 20, rating: 5 },
     { id: 11, course: "NodeJS", price: 20, rating: 5 },
   ]);
-  const [update, setUpdate]= useState(0)
-  
-  const[searchValue, setSearchValue]= useState("")
-  const [filter, setFilter]= useState([])
+  const [update, setUpdate] = useState(0);
+
+  const [searchValue, setSearchValue] = useState("");
+  const [filter, setFilter] = useState([]);
 
   const handleOpen = () => setOpen(true);
-  const handleClose = () =>setOpen(false);
+  const handleClose = () => setOpen(false);
   const handleColumnName = (e) => setColumnName(e.target.value);
 
   const handleColumn = () => {
-    if(columnName.length<=1){
-      handleClose()
-    }
-    else{
-    setColumns([...columns, columnName]); 
-    setColumnName("")
-    handleClose();
+    if (columnName.length <= 1) {
+      handleClose();
+    } else {
+      setColumns([...columns, columnName]);
+      setColumnName("");
+      handleClose();
     }
   };
 
-  
-  const handleSearch= ((e)=>{
-    setSearchValue(e.target.value)
+  const handleSearch = (e) => {
+    setSearchValue(e.target.value);
     let searched = rows.filter((ser) => {
       if (ser.course) {
-        return ser.course.toLowerCase().includes(searchValue.toLocaleLowerCase());
+        return ser.course
+          .toLowerCase()
+          .includes(searchValue.toLocaleLowerCase());
       }
     });
     setFilter(searched);
-  })
+  };
 
-  const handleDelete =(id)=>{
-  let filteredRow= rows.filter((item,index)=>{
-      return id!==index
-    })
-    setRows(filteredRow)
-  }
+  const handleDelete = (id) => {
+    let filteredRow = rows.filter((item, index) => {
+      return id !== index;
+    });
+    setRows(filteredRow);
+  };
 
-  const handleSort = ()=>{
-   let sortedRow= rows.sort((a,b)=>{
-      return a.rating-b.rating
-    })
+  const handleSort = () => {
+    let sortedRow = rows.sort((a, b) => {
+      return a.rating - b.rating;
+    });
     console.log(sortedRow);
-    setRows(sortedRow)
-    setUpdate((x)=>x+1)
-  }
+    setRows(sortedRow);
+    setUpdate((x) => x + 1);
+  };
 
   return (
-    <TableContainer component={Paper}> 
-      <input type="text" onChange={handleSearch} placeholder="Search" value={searchValue} name="" id="" />
+    <TableContainer component={Paper}>
+      
+      <h1>React Table</h1>  
+      <input
+        type="text"
+        className="input_search"
+        onChange={handleSearch}
+        placeholder="Search by Course"
+        value={searchValue}
+        name=""
+        id=""
+      />
       <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
         <TableHead>
           <TableRow>
             {columns.map((item) => {
-              if(item==="Rating"){
-                return <TableCell>{item} <button  className="sortButton"><ImportExportIcon onClick={handleSort}/></button></TableCell>
+              if (item === "Rating") {
+                return (
+                  <TableCell>
+                    {item}{" "}
+                    <button onClick={handleSort} className="sortButton">
+                      Sort
+                    </button>
+                  </TableCell>
+                );
               }
-              return <TableCell>{item}</TableCell>
+              return <TableCell>{item}</TableCell>;
             })}
             <TableCell>
               {" "}
               <React.Fragment>
-                <Button className="column_button" onClick={handleOpen}>Add column</Button>
+                <Button className="column_button" onClick={handleOpen}>
+                  Add column
+                </Button>
                 <Modal
                   hideBackdrop
                   open={open}
@@ -117,7 +140,11 @@ export default function DataTable() {
                   aria-describedby="child-modal-description"
                 >
                   <Box sx={{ ...style, width: 200 }}>
-                    <input type="text" placeholder="Enter name of Column" onChange={handleColumnName} />
+                    <input
+                      type="text"
+                      placeholder="Enter name of Column"
+                      onChange={handleColumnName}
+                    />
                     <Button onClick={handleColumn}>Done</Button>
                   </Box>
                 </Modal>
@@ -126,35 +153,47 @@ export default function DataTable() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {
-            searchValue!==""?
-          filter.map((filter, index) => (
-            <TableRow
-              key={filter.name}
-              id={index}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell>{filter.id}</TableCell>
-              <TableCell>{filter.course}</TableCell>
-              <TableCell>{filter.price}</TableCell>
-              <TableCell>{filter.rating}</TableCell>
-              <TableCell><button className="delete" onClick={()=>handleDelete(index)}>Delete</button></TableCell>
-            </TableRow>
-          )):
-          rows.map((row, index) => (
-            <TableRow
-              key={filter.name}
-              id={index}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell>{row.id}</TableCell>
-              <TableCell>{row.course}</TableCell>
-              <TableCell>{row.price}</TableCell>
-              <TableCell>{row.rating}</TableCell>
-              <TableCell><button  className="delete"  onClick={()=>handleDelete(index)}>Delete</button></TableCell>
-            </TableRow>
-          ))}
-          
+          {searchValue !== ""
+            ? filter.map((filter, index) => (
+                <TableRow
+                  key={filter.name}
+                  id={index}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell>{filter.id}</TableCell>
+                  <TableCell>{filter.course}</TableCell>
+                  <TableCell>{filter.price}</TableCell>
+                  <TableCell>{filter.rating}</TableCell>
+                  <TableCell>
+                    <button
+                      className="delete"
+                      onClick={() => handleDelete(index)}
+                    >
+                      Delete
+                    </button>
+                  </TableCell>
+                </TableRow>
+              ))
+            : rows.map((row, index) => (
+                <TableRow
+                  key={filter.name}
+                  id={index}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell>{row.id}</TableCell>
+                  <TableCell>{row.course}</TableCell>
+                  <TableCell>{row.price}</TableCell>
+                  <TableCell>{row.rating}</TableCell>
+                  <TableCell>
+                    <button
+                      className="delete"
+                      onClick={() => handleDelete(index)}
+                    >
+                      Delete
+                    </button>
+                  </TableCell>
+                </TableRow>
+              ))}
         </TableBody>
       </Table>
     </TableContainer>
